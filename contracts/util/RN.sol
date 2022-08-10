1a980af67fad7b236d57b39390f92c4db6dfa247
bingo <bingo_zwb@163.com>
2022-08-10 17:50:45 +0800

    Add get rnb within _range

--- contracts/util/RN.sol
+++ contracts/util/RN.sol
@@ -17,6 +17,15 @@ contract RN {
     }
 
     /**
+     * @dev get fake random number
+     * @return return rnb range [1, _range]
+     */
+    function getRandomNumber(uint _range) internal returns (uint) {
+        bytes32 result = keccak256(abi.encodePacked(seed++, msg.sender, block.timestamp, block.coinbase, gasleft()));
+        return uint(result % _range + 1);
+    }
+
+    /**
      * @dev random number within weight
      */
     function randomWeight(uint[] memory _weightList) internal returns (uint) {
