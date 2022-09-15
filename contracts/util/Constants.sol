245f4a99bf6c68d0c871d6d2b1549a562a3ca4e2
bingo <bingo_zwb@163.com>
2022-09-15 17:08:05 +0800

    Update util/Constants.sol

--- contracts/util/Constants.sol
+++ contracts/util/Constants.sol
@@ -6,13 +6,10 @@ pragma solidity ^0.8.0;
 library Constants {
     address payable constant BURN_ADDRESS = payable(0x000000000000000000000000000000000000dEaD);
 
-    uint constant TREE_ID = 210001;
-    uint constant FRUIT_ID = 220001;
-    uint constant FISH_ID = 240013;
+    uint constant C1_ID = 210001;
 
-
-    function FIELD_IDS() internal pure returns (uint[] memory res) {
-        uint[8] memory const = [uint(271001), 271002, 272001, 272002, 272003, 272004, 272005, 272006];
+    function C1_IDS() internal pure returns (uint[] memory res) {
+        uint[3] memory const = [uint(210001), 210002, 210003];
         res = new uint[](const.length);
         for (uint i = 0; i < res.length; i++) {res[i] = const[i];}
     }
