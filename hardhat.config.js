0548cf556a9e90ca7b51b4b5f7f645f0f1280cad
bingo <bingo_zwb@163.com>
2022-11-15 15:53:34 +0800

    Add hardhat-etherscan plugin

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,20 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle')
 
 // hardhat upgrade plugins
-require('@nomiclabs/hardhat-ethers');
-require('@openzeppelin/hardhat-upgrades');
+require('@nomiclabs/hardhat-ethers')
+require('@openzeppelin/hardhat-upgrades')
+// hardhat etherscan plugins
+require('@nomiclabs/hardhat-etherscan')
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners()
 
   for (const account of accounts) {
-    console.log(account.address);
+    console.log(account.address)
   }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -22,16 +24,33 @@ task("accounts", "Prints the list of accounts", async () => {
  */
 module.exports = {
   solidity: {
-    version: "0.8.4",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+    compilers: [
+      {
+        version: '0.5.16',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
+      },
+      {
+        version: '0.8.4',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
       }
-    }
+    ]
   },
   networks: {
     hardhat: {
+      forking: { // https://hardhat.org/hardhat-network/docs/guides/forking-other-networks
+        url: '',
+        enabled: false,
+      },
       accounts: {
         mnemonic: ''
       },
@@ -70,5 +89,30 @@ module.exports = {
       accounts: [],
       gas: 'auto',
     },
+    goerli: {
+      url: 'https://goerli.infura.io/v3/',
+      accounts: {
+        mnemonic: '',
+        initialIndex: 0
+      },
+      gas: 'auto',
+    },
+    eth: {
+      url: 'https://mainnet.infura.io/v3/',
+      accounts: {
+        mnemonic: '',
+        initialIndex: 0
+      },
+      gas: 'auto',
+    },
   },
-};
+  etherscan: { // https://hardhat.org/hardhat-runner/plugins/nomiclabs-hardhat-etherscan#hardhat-etherscan
+    // Your API key for Etherscan
+    // Obtain one at https://bscscan.com/
+    apiKey: {
+      mainnet: '',
+      bsc: '',
+      bscTestnet: ''
+    }
+  },
+}
