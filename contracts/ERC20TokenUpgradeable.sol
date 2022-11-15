1ff905252e6606b8d9cc41434f2bd18ef3b6a568
bingo <bingo_zwb@163.com>
2022-11-15 16:12:15 +0800

    Fix ERC20Upgradeable.sol typo

--- contracts/ERC20TokenUpgradeable.sol
+++ contracts/ERC20TokenUpgradeable.sol
@@ -9,12 +9,12 @@ contract ERC20TokenUpgradeable is ERC20Upgradeable {
 
     uint8 _decimals;
 
-    function initialize(string memory name_, string memory symbol_, uint8 decimals_, uint initalSupply_) public initializer {
+    function initialize(string memory name_, string memory symbol_, uint8 decimals_, uint initialSupply_) public initializer {
         __ERC20_init(name_, symbol_);
 
         _decimals = decimals_;
 
-        _mint(msg.sender, initalSupply_ * 10 ** decimals());
+        _mint(msg.sender, initialSupply_ * 10 ** decimals());
     }
 
     function decimals() public view override returns (uint8) {
